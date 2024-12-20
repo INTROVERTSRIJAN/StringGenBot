849ec9c7e51a85ef797a318eea13527131a98ae5
SRIJAN <141845601+INTROVERTSRIJAN@users.noreply.github.com>
2024-12-20 12:58:56 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nɪ ᴄᴀɴ ᴘʏʀᴏɢʀᴀᴍ ᴠ𝟷 , ᴠ𝟸 ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ sᴇssɪᴏɴ sᴛʀɪɴɢ ғᴏʀ ʏᴏᴜ. ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ sᴛᴀʀᴛ..",
+        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ ᴠ𝟷 , ᴠ𝟸 ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ sᴇssɪᴏɴ sᴛʀɪɴɢ ғᴏʀ ʏᴏᴜ. ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ sᴛᴀʀᴛ..",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
